c9d18fc4ae3f30f226f2b28d557a864746b7f014
jerry <jsd12060123@163.com>
2021-03-29 00:04:14 +0800

    更新数据

--- pages/search/index.js
+++ pages/search/index.js
@@ -16,31 +16,57 @@ Page({
     goods:[],
     // 取消 按钮 是否显示
     isFocus:false,
+    hidderAuthPhone: true,
     // 输入框的值
-    inpValue:""
+    inpValue:"",
+    checked: false
   },
   TimeId:-1,
+  onShow() {
+
+  },
   // 输入框的值改变 就会触发的事件
   handleInput(e){
-    // 1 获取输入框的值
-    const {value}=e.detail;
-    // 2 检测合法性
-    if(!value.trim()){
-      this.setData({
-        goods:[],
-        isFocus:false
-      })
-      // 值不合法
-      return;
-    }
-    // 3 准备发送请求获取数据
-    this.setData({
-      isFocus:true
-    })
-    clearTimeout(this.TimeId);
-    this.TimeId=setTimeout(() => {
-      this.qsearch(value);
-    }, 1000);
+    const  _this = this
+    const user = wx.getStorageSync('USER')
+    request({ url: "/member/" + user.id })
+        .then(result => {
+          if (result.data && result.data.status === 1) {
+            _this.setData({
+              checked: true
+            })
+            // 1 获取输入框的值
+            const {value}=e.detail;
+            // 2 检测合法性
+            if(!value.trim()){
+              this.setData({
+                goods:[],
+                isFocus:false
+              })
+              // 值不合法
+              return;
+            }
+            // 3 准备发送请求获取数据
+            this.setData({
+              isFocus:true
+            })
+            clearTimeout(this.TimeId);
+            this.TimeId=setTimeout(() => {
+              this.qsearch(value);
+            }, 1000);
+          }else {
+            if(result.data.phone && result.data.status === 0) {
+              wx.showToast({
+                title: '很抱歉，您还未审核通过哦',
+                icon: 'none'
+              })
+              return false
+            }
+            this.setData({
+              hidderAuthPhone: false
+            })
+          }
+        })
   },
   // 发送请求获取搜索建议 数据
   async qsearch(query){
@@ -50,6 +76,18 @@ Page({
         goods:result.data.list
       })
     });
+
+  },
+  getPhoneNumber (e) {
+    wx.showToast({
+      title: '申请已提交，请耐心等待审核结果',
+      icon: 'none'
+    })
+    const user = wx.getStorageSync('USER')
+    this.setData({
+      user: user
+    })
+
   },
   // 点击 取消按钮
   handleCancel(){
@@ -58,5 +96,36 @@ Page({
       isFocus:false,
       goods:[]
     })
+  },
+  toDetail(e) {
+    const user = wx.getStorageSync('USER')
+    let checked = false
+    if(user && user.id) {
+      request({ url: "/member/" + user.id })
+          .then(result => {
+            if(!result.data.phone) {
+              this.setData({
+                hidderAuthPhone: false
+              })
+              return false
+            }
+            if(result.data && result.data.status===1) {
+              checked = true
+            }
+            if(!checked) {
+              wx.showToast({
+                title: '很抱歉，您还未审核通过哦',
+                icon: 'none'
+              })
+              return false
+            }
+            if(checked) {
+              const goodId = e.currentTarget.dataset.id
+              wx.navigateTo({
+                url:'/pages/goods_detail/index?goods_id=' + goodId
+              })
+            }
+          })
+    }
   }
 })
